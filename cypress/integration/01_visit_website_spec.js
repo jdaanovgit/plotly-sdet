a346d5d085d928da895bf0c5633e7e557aeba3d0
Alexander <jdaanov@gmail.com>
2024-01-16 14:49:31 -0800

    Update 01_visit_website_spec.js
    Users are able to visit the website and able to scroll down to “Loved by OSS,
    trusted by Enterprise” and see the weekly downloads number.

--- cypress/integration/01_visit_website_spec.js
+++ cypress/integration/01_visit_website_spec.js
@@ -1,1 +1,14 @@
+describe('Cypress Website Test', () => {
+  it('should scroll to "Loved by OSS, trusted by Enterprise" and verify weekly downloads', () => {
+    // Visit the Cypress website
+    cy.visit('https://www.cypress.io/');
 
+    // Scroll to the "Loved by OSS, trusted by Enterprise" section
+    cy.contains('Loved by').scrollIntoView();
+
+    // Verify the weekly downloads number is visible
+    cy.contains('Weekly downloads').should('be.visible');
+    // Additional check, if the section does exist but the number itself is not
+    cy.contains('5M+').should('be.visible');
+  });
+});
