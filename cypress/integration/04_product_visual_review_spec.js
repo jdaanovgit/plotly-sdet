6a842eb1d360f56c74b422c09f62c3d466963380
Alexander <jdaanov@gmail.com>
2024-01-16 15:24:53 -0800

    Update 04_product_visual_review_spec.js
    User is able to click on “Product” and then “Visual reviews”

--- cypress/integration/04_product_visual_review_spec.js
+++ cypress/integration/04_product_visual_review_spec.js
@@ -1,1 +1,18 @@
+describe('Navigate to Visual Reviews', () => {
+  it('should click on Product and then on Visual Reviews', () => {
+    // Visit the Cypress website
+    cy.visit('https://www.cypress.io/');
 
+    // Click on the "Product" menu item
+    cy.contains('Product').click();
+
+    // Click on the "Visual reviews" sub-menu item
+    cy.contains('Visual reviews').click();
+
+    // Verify that the page has navigated to the "Visual reviews" section
+    cy.url().should('include', '/cloud#visual_reviews');
+
+    // Optionally, you can add more assertions to verify content on the "Visual reviews" page
+    cy.contains('Powerful Visual Testing').should('be.visible');
+  });
+});
