c66932efd5c66d8a98ff35246930a72b1a0073a7
Alexander <jdaanov@gmail.com>
2024-01-16 15:20:36 -0800

    Update 03_install_npm_spec.js
    User is able to click on “Install” and then on “npm install cypress” and make sure
    the copied text is “npm install cypress --save-dev”

--- cypress/integration/03_install_npm_spec.js
+++ cypress/integration/03_install_npm_spec.js
@@ -1,1 +1,18 @@
+describe('Verify Install Command', () => {
+  it('should click on Install and verify copied text', () => {
+    // Visit the Cypress website
+    cy.visit('https://www.cypress.io/');
 
+    // Click on the "Install" button
+    cy.contains('npm install cypress').click();
+
+    // Click on "npm install cypress"
+    cy.contains('npm install cypress').click();
+
+    // Verify that the copied text is "npm install cypress --save-dev"
+    cy.get('.copy-code-action').click();
+
+    // Get the copied text from the clipboard and assert 
+    cy.window().invoke('navigator.clipboard.readText').should('eq', 'npm install cypress --save-dev');
+  });
+});
