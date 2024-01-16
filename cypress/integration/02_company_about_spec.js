e771fc47a0f5528a502bf70797af0e162de7b6e5
Alexander <jdaanov@gmail.com>
2024-01-16 15:13:53 -0800

    Update 02_company_about_spec.js
    User is able to click on Company and then on “About Cypress” (from the menu)

--- cypress/integration/02_company_about_spec.js
+++ cypress/integration/02_company_about_spec.js
@@ -1,1 +1,18 @@
+describe('Navigate to About Cypress', () => {
+  it('should click on Company from the menu and then on About Cypress, as a sub-menu item', () => {
+    // Visit the Cypress website
+    cy.visit('https://www.cypress.io/');
 
+    // Click on the "Company" menu item
+    cy.get('#dropdownCompany').click();
+
+    // Click on the "About Cypress" sub-menu item
+    cy.contains('About Cypress').click();
+
+    // Verify that the page has navigated to the "About Cypress" section
+    cy.url().should('include', '/about-us');
+
+    // Optionally, another assertion to verify content on the "About Cypress" page
+    cy.contains('Cypress testing tools support developers all around the world').should('be.visible');
+  });
+});
