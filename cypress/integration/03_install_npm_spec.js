380211cf507b23816bc8d3123dae957fe7e80627
Alexander <jdaanov@gmail.com>
2024-01-16 19:44:42 -0800

    Update 03_install_npm_spec.js

--- cypress/integration/03_install_npm_spec.js
+++ cypress/integration/03_install_npm_spec.js
@@ -3,11 +3,11 @@ describe('Verify Install Command', () => {
     // Visit the Cypress website
     cy.visit('https://www.cypress.io/');
 
-    // Click on the "Install" button
+    // Click on the "Install" button from home page
     cy.contains('npm install cypress').click();
 
-    // Click on "npm install cypress"
-    cy.contains('npm install cypress').click();
+    // Click on "npm install cypress" from popup/div
+    cy.contains('[data-cy="modal-install-copy"]').click();
 
     // Verify that the copied text is "npm install cypress --save-dev"
     cy.get('.copy-code-action').click();
