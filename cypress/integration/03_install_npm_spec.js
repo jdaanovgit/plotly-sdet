dc1ba3e48c0634480fcc2cc09ed70039f1ab7093
Alexander <jdaanov@gmail.com>
2024-01-16 20:02:14 -0800

    Update 03_install_npm_spec.js

--- cypress/integration/03_install_npm_spec.js
+++ cypress/integration/03_install_npm_spec.js
@@ -4,15 +4,20 @@ describe('Verify Install Command', () => {
     cy.visit('https://www.cypress.io/');
 
     // Click on the "Install" button from home page
-    cy.contains('npm install cypress').click();
+    cy.contains(' npm install cypress ').click();
 
     // Click on "npm install cypress" from popup/div
-    cy.contains('[data-cy="modal-install-copy"]').click();
+    //cy.contains('[data-cy="modal-install-copy"]').click();
+    cy.contains('npm install cypress').click();
 
     // Verify that the copied text is "npm install cypress --save-dev"
-    cy.get('.copy-code-action').click();
+    cy.window().then((win) => {
+    win.navigator.clipboard.readText().then((text) => {
+      expect(text).to.eq('npm install cypress --save-dev');
+    });
+  });
 
     // Get the copied text from the clipboard and assert 
-    cy.window().invoke('navigator.clipboard.readText').should('eq', 'npm install cypress --save-dev');
+    //cy.window().invoke('navigator.clipboard.readText').should('eq', 'npm install cypress --save-dev');
   });
 });
